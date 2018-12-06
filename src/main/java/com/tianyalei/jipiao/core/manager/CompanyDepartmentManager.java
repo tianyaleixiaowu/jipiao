5ac513c89430836c32f6b9d610371a9418835d22
wuwf <272551766@qq.com>
2018-12-06 14:41:03 +0800

    1

--- src/main/java/com/tianyalei/jipiao/core/manager/CompanyDepartmentManager.java
+++ src/main/java/com/tianyalei/jipiao/core/manager/CompanyDepartmentManager.java
@@ -2,13 +2,17 @@ package com.tianyalei.jipiao.core.manager;
 
 import com.tianyalei.jipiao.core.model.MCompanyDepartmentEntity;
 import com.tianyalei.jipiao.core.repository.CompanyDepartmentRepository;
+import com.tianyalei.jipiao.core.response.CompanyDepartmentResponseVO;
 import com.tianyalei.jipiao.global.bean.SimplePage;
+import com.tianyalei.jipiao.global.cache.DictCache;
+import com.xiaoleilu.hutool.util.BeanUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.stream.Collectors;
 
 /**
  * @author wuweifeng wrote on 2018/11/1.
@@ -17,6 +21,8 @@ import javax.annotation.Resource;
 public class CompanyDepartmentManager {
     @Resource
     private CompanyDepartmentRepository companyDepartmentRepository;
+    @Resource
+    private DictCache dictCache;
 
     public String findName(Integer deptId) {
         try {
@@ -53,14 +59,20 @@ public class CompanyDepartmentManager {
         update(entity);
     }
 
-    public SimplePage<MCompanyDepartmentEntity> findByCompanyId(Integer companyId, int pp, int size) {
+    public SimplePage<CompanyDepartmentResponseVO> findByCompanyId(Integer companyId, int pp, int size) {
         if (size == 0) {
             size = 10;
         }
         Pageable pageable = PageRequest.of(pp, size);
         Page<MCompanyDepartmentEntity> page = companyDepartmentRepository.findByCompanyIdAndIsEnableTrue(companyId, pageable);
         return new SimplePage<>(page.getTotalPages(), page
-                .getTotalElements(), page.getContent());
+                .getTotalElements(), page.getContent().stream().map(this::parse).collect(Collectors.toList()));
     }
 
+    private CompanyDepartmentResponseVO parse(MCompanyDepartmentEntity entity) {
+        CompanyDepartmentResponseVO vo = new CompanyDepartmentResponseVO();
+        BeanUtil.copyProperties(entity, vo);
+        vo.setApproveTypeValue(dictCache.findByGroupIdAndKey(61, entity.getApproveType()));
+        return vo;
+    }
 }
---
+++ src/main/java/com/tianyalei/jipiao/core/response/CompanyDepartmentResponseVO.java
@@ -0,0 +1,83 @@
+package com.tianyalei.jipiao.core.response;
+
+/**
+ * @author wuweifeng wrote on 2018/12/6.
+ */
+public class CompanyDepartmentResponseVO {
+    private Long id;
+    private int companyId;
+    private String departmentName;
+    private boolean isDeptApprove;
+    /**
+     * groupId=61
+     */
+    private String approveType;
+    private String approveTypeValue;
+
+    private String hrDeptId = "";
+    private boolean isEnable = true;
+
+    public String getApproveTypeValue() {
+        return approveTypeValue;
+    }
+
+    public void setApproveTypeValue(String approveTypeValue) {
+        this.approveTypeValue = approveTypeValue;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public boolean isDeptApprove() {
+        return isDeptApprove;
+    }
+
+    public void setDeptApprove(boolean deptApprove) {
+        isDeptApprove = deptApprove;
+    }
+
+    public String getApproveType() {
+        return approveType;
+    }
+
+    public void setApproveType(String approveType) {
+        this.approveType = approveType;
+    }
+
+    public String getHrDeptId() {
+        return hrDeptId;
+    }
+
+    public void setHrDeptId(String hrDeptId) {
+        this.hrDeptId = hrDeptId;
+    }
+
+    public boolean getIsEnable() {
+        return isEnable;
+    }
+
+    public void setIsEnable(boolean enable) {
+        isEnable = enable;
+    }
+}
